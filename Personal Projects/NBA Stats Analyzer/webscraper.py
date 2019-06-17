cbfa0a33cec7cbf8e2527094c5012312fd3d660e
Felipe Canales <FelipeCanales@Felipes-MacBook-Pro.local>
2019-06-16 21:04:56 -0400

    worked on webscraper.scraped all NBA team rosters for the 2018-2019 season

--- Personal Projects/NBA Stats Analyzer/webscraper.py
+++ Personal Projects/NBA Stats Analyzer/webscraper.py
@@ -1,74 +1,77 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-import csv, os
+import csv, os, time
 from os.path import join as pjoin
 
 # TODO:!!!! IMPORTANT !!!! Account for all edge cases
 # TODO: Separate away games from home games.
 # TODO: Clean up data. No need for Date, Age, ...etc
-# TODO: Figure out a way to organize by creating a new Folder labeled with the Team's Name. Within that folder,
-#       have team stats, individual stats
 # TODO: Organize organize organize! Still have a long way to go
-# TODO: create function that grabs all team stats
 # TODO: find a way to get all shots taken by a player/team
 
 
 # global #
 # team abbreviations to get game longs
 team_abbreviations = {'Atlanta Hawks': 'ATL',
-                     'Brooklyn Nets': 'BRK',
-                     'Boston Celtics': 'BOS',
-                     'Charlotte Hornets': 'CHA',
-                     'Chicago Bulls': 'CHI',
-                     'Cleveland Cavaliers': 'CLE',
-                     'Dallas Mavericks': 'DAL',
-                     'Denver Nuggets': 'DEN',
-                     'Detroit Pistons': 'DET',
-                     'Golden State Warriors': 'GSW',
-                     'Houston Rockets': 'HOU',
-                     'Indiana Pacers': 'IND',
-                     'Los Angeles Clippers': 'LAC',
-                     'Los Angeles Lakers': 'LAL',
-                     'Memphis Grizzlies': 'MEM',
-                     'Miami Heat': 'MIA',
-                     'Milwaukee Bucks': 'MIL',
-                     'Minnesota Timberwolves': 'MIN',
-                     'New Orleans Pelicans': 'NOP',
-                     'New York Knicks': 'NYK',
-                     'Oklahoma City Thunder': 'OKC',
-                     'Orlando Magic': 'ORL',
-                     'Philadelphia 76ers': 'PHI',
-                     'Phoenix Suns': 'PHX',
-                     'Portland Trail Blazers': 'POR',
-                     'Sacramento Kings': 'SAC',
-                     'San Antonio Spurs': 'SAS',
-                     'Toronto Raptors': 'TOR',
-                     'Utah Jazz': 'UTA',
-                     'Washington Wizards': 'WAS'}
+                      'Brooklyn Nets': 'BRK',
+                      'Boston Celtics': 'BOS',
+                      'Charlotte Hornets': 'CHO',
+                      'Chicago Bulls': 'CHI',
+                      'Cleveland Cavaliers': 'CLE',
+                      'Dallas Mavericks': 'DAL',
+                      'Denver Nuggets': 'DEN',
+                      'Detroit Pistons': 'DET',
+                      'Golden State Warriors': 'GSW',
+                      'Houston Rockets': 'HOU',
+                      'Indiana Pacers': 'IND',
+                      'Los Angeles Clippers': 'LAC',
+                      'Los Angeles Lakers': 'LAL',
+                      'Memphis Grizzlies': 'MEM',
+                      'Miami Heat': 'MIA',
+                      'Milwaukee Bucks': 'MIL',
+                      'Minnesota Timberwolves': 'MIN',
+                      'New Orleans Pelicans': 'NOP',
+                      'New York Knicks': 'NYK',
+                      'Oklahoma City Thunder': 'OKC',
+                      'Orlando Magic': 'ORL',
+                      'Philadelphia 76ers': 'PHI',
+                      'Phoenix Suns': 'PHO',
+                      'Portland Trail Blazers': 'POR',
+                      'Sacramento Kings': 'SAC',
+                      'San Antonio Spurs': 'SAS',
+                      'Toronto Raptors': 'TOR',
+                      'Utah Jazz': 'UTA',
+                      'Washington Wizards': 'WAS'}
+
 
 # web scraping function get team stats for all 82 regular season games and write to a csv file
 def getTeamStats(team_name):
-    placeholder = ''
+    url = 'https://www.basketball-reference.com/boxscores/201810170DET.html'    # url for boxscores
+    url2 = 'https://www.basketball-reference.com/teams/BRK/2019_games.html'     # url for schedule
+    # 1: Have to separate 'opp' and date from team schedule
+    # 2: format it such that it's yyyymmdd0TEAM_Abbreviation
+    # 3: webscrape Team Totals
+    # 4: find a way to get shot attempt coords from the game
+    # 4: save game id and team totals to one csv and shot attempt coords to another csv
+
 
 
 # web scraping function to get an individual player's stats for all 82 regular season games and write to a csv file
 def individualStats(player_name):
-
-
-    player_abb = getNameAbb(player_name)    # must get abbreviation used by basketball-reference
+    player_abb = getNameAbb(player_name)  # must get abbreviation used by basketball-reference
     url = 'https://www.basketball-reference.com/players/{}/{}/gamelog/2019'.format(player_abb[0], player_abb)
     bsoup = soup(uReq(url), 'html.parser')  # use BeautifulSoup in order to parse through site
 
     # Get player's team name and store their data in their respective folder #
     player_team = getPlayerTeam(bsoup)
-    createFolder(player_team + '/individual_stats')   # creates new directory using team's name if it doesn't exist
-    directory_name = pjoin(player_team + '/individual_stats', str(getPlayerNumber(player_name) + '.csv')) # puts csv in team directory
+    createFolder(player_team + '/individual_stats')  # creates new directory using team's name if it doesn't exist
+    directory_name = pjoin(player_team + '/individual_stats', str(getPlayerNumber(player_name) + '.csv'))  # puts csv in team directory
     csv_writer = csv.writer(open(directory_name, 'w'))  # opens stream to write into csv
 
     # Find and iterate through player's data #
 
-    table = bsoup.find("tbody")     # get information on table
-    num_rows = len(table.findAll('tr'))     # find number of rows
+    table = bsoup.find("tbody")  # get information on table
+    num_rows = len(table.findAll('tr'))  # find number of rows
 
     # get table header and write to csv
     header = [th.getText() for th in bsoup.find('thead').findAll('tr', limit=2)[0].findAll('th')][1:]
@@ -90,7 +93,7 @@ def getPlayerNumber(player_name):
     with open('Brooklyn_Nets/nets_players.csv', mode='r') as file:
         read_csv = csv.DictReader(file)
         for x in read_csv:
-            if(x.get('First') == player_name.split(' ')[0] and x.get('Last') == player_name.split(' ')[1]):
+            if x.get('First') == player_name.split(' ')[0] and x.get('Last') == player_name.split(' ')[1]:
                 return x.get('\ufeffNum')
 
 
@@ -109,10 +112,52 @@ def getPlayerTeam(bsoup):
     return player_team
 
 
-# test
+# Creates player database for every NBA team.
+def getTeamRoster(team_name):
+    if team_name in team_abbreviations:  # Make sure it's a valid NBA team
+        team_name_fixed = team_name.replace(' ', '_')  # Replace spaces with underscores
+        createFolder(team_name_fixed)  # creates team folder if it isn't there already
+        directory = team_name_fixed + '/' + team_name_fixed + '_roster.csv'  # directory to write csv
+
+        # start web scraping
+        url = 'https://www.basketball-reference.com/teams/{}/2019.html'.format(team_abbreviations.get(team_name))
+        bsoup = soup(uReq(url), 'html.parser')
+
+        header = ['Num', 'First', 'Last', 'Pos']  # only need this info from players
+
+        table = bsoup.find('tbody')  # get table info
+        num_rows = len(table.findAll('tr'))  # find number of rows
+
+        csv_writer = csv.writer(open(directory, 'w'))  # opens stream to write into csv
+        csv_writer.writerow(header)  # write header into
+
+        # get all players's info and write into a csv
+        for row in range(num_rows):
+            player_num = [th.getText() for th in table.findAll('tr')[row].findAll('th')]  # gets player's number
+            players = player_num + [th.getText() for th in table.findAll('tr')[row].findAll('td')][0:3]  # everything else
+
+            # Must check whether or not a player has a first and last name (ex. Nene from the Rockets only has 1 name)
+            if ' ' not in players[1]:
+                players[3] = players[2]
+                players[2] = ''
+            else:
+                name = players[1].split()  # player name must be split into two parts - First and Last
+                players[3] = players[2]  # shift info one index to the right
+                players[1] = name[0]
+                players[2] = name[1]
+                csv_writer.writerow(players)  # write info onto a csv
+
+    else:
+        raise TypeError('Not an NBA team!')
+
+
+# testing
 individualStats('D\'Angelo Russell')
 individualStats('Joe Harris')
 individualStats('Caris Levert')
+
+for x in team_abbreviations.keys():
+    getTeamRoster(x)
 
 
 
